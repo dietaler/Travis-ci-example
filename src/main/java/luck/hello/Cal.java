c741aed15bfd053aa4198cd5f4e8251bfe8baebc
dietaler <tomtom21107@gmail.com>
2022-01-18 11:06:58 +0800

    init

---
+++ src/main/java/luck/hello/Cal.java
@@ -0,0 +1,13 @@
+package luck.hello;
+
+/**
+ * Hello world!
+ *
+ */
+public class Cal 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/luck/hello/TestAdd.java
@@ -0,0 +1,7 @@
+package luck.hello;
+
+import junit.framework.TestCase;
+
+public class TestAdd extends TestCase {
+
+}
